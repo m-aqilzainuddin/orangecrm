d4e15791213064827fe2134d09b15e01de61c04d
m-aqilzainuddin <muhdaqilzainuddin@gmail.com>
2023-11-11 21:59:13 +0800

    Update - update AdminPage script

--- src/main/java/pages/AdminPage.java
+++ src/main/java/pages/AdminPage.java
@@ -1,10 +1,14 @@
 package pages;
 
-import base.TestBase;
+import org.openqa.selenium.WebDriver;
+import utils.TestBase;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-public class AdminPage extends TestBase {
+public class AdminPage {
+
+    WebDriver driver;
+
     @FindBy(xpath = "//a[contains(@href, 'viewAdminModule')]")
     WebElement adminLink;
 
@@ -13,6 +17,9 @@ public class AdminPage extends TestBase {
 
     @FindBy(xpath = "//div[text()='Admin']")
     WebElement adminUsernameResult;
+
+    @FindBy (xpath = "//p[text()='No Records Found']")
+    WebElement adminErrorUsername;
 
     @FindBy(xpath = "//button[text()=\" Search \"]")
     WebElement adminSearchButton;
@@ -41,10 +48,11 @@ public class AdminPage extends TestBase {
     @FindBy(xpath = "//button[text()=\" Save \"]")
     WebElement adminSaveButton;
 
-    @FindBy(xpath = "//button[text()=\" Cancel \"]")
+    @FindBy(xpath = "//button[text()=\' Cancel \']")
     WebElement adminCancelButton;
 
-    public AdminPage(){
+    public AdminPage(WebDriver driver){
+        this.driver = driver;
         PageFactory.initElements(driver, this);
     }
 
@@ -55,16 +63,23 @@ public class AdminPage extends TestBase {
         adminLink.click();
     }
     public void enterSystemUsername(String username){
+        adminUsername.click();
         adminUsername.sendKeys(username);
     }
     public void clickSearchButton(){
         adminSearchButton.click();
+    }
+    public void clickResetButton(){
+        adminResetButton.click();
     }
     public void clickCancelButton(){
         adminCancelButton.click();
     }
     public boolean usernameDisplayed(){
         return adminUsernameResult.isDisplayed();
+    }
+    public boolean usernameNotDisplayed(){
+        return adminErrorUsername.isDisplayed();
     }
 
 }
