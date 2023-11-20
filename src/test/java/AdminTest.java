bc44d98b8be189c41fe3c7fff277fb93712270d6
m-aqilzainuddin <muhdaqilzainuddin@gmail.com>
2023-11-20 16:41:59 +0800

    Update - add test scenarios in script

--- src/test/java/AdminTest.java
+++ src/test/java/AdminTest.java
@@ -53,4 +53,83 @@ public class AdminTest extends BaseTest {
         boolean usernameDisplayed = adminPage.usernameDisplayed();
         assertTrue(usernameDisplayed);
     }
+
+    //unable to add new job title "form incomplete"
+    @Test
+    public void failAddJobTitle(){
+        boolean displayAdminLink = adminPage.checkAdminLink();
+        assertTrue(displayAdminLink);
+        adminPage.clickAdminLink();
+        adminPage.clickJobBtn();
+        adminPage.clickJobTitleLink();
+        adminPage.clickJobTitleAddBtn();
+        adminPage.inputJobDescInput("Responsible for monitoring and improve an organization's final products.");
+        adminPage.clickSaveBtn();
+        boolean jobTitleError = adminPage.errorDisplayed();
+        assertTrue(jobTitleError);
+    }
+    @Test
+    public void successfulAddJobTitle(){
+        boolean displayAdminLink = adminPage.checkAdminLink();
+        assertTrue(displayAdminLink);
+        adminPage.clickAdminLink();
+        adminPage.clickJobBtn();
+        adminPage.clickJobTitleLink();
+        adminPage.clickJobTitleAddBtn();
+        adminPage.inputJobTitle("Quality Assurance");
+        adminPage.clickSaveBtn();
+        boolean successfullyAddedMsg = adminPage.verifySuccessAddMsg();
+        assertTrue(successfullyAddedMsg);
+        adminPage.clickJobBtn();
+        adminPage.clickJobTitleLink();
+        boolean verifyJobTitleDisplayed = adminPage.verifyJobTitleExistance();
+        assertTrue(verifyJobTitleDisplayed);
+    }
+    @Test
+    public void cancelAddJobTitle(){
+        boolean displayAdminLink = adminPage.checkAdminLink();
+        assertTrue(displayAdminLink);
+        adminPage.clickAdminLink();
+        adminPage.clickJobBtn();
+        adminPage.clickJobTitleLink();
+        adminPage.clickJobTitleAddBtn();
+        adminPage.inputJobTitle("Quality Assurance");
+        adminPage.clickCancelButton();
+        adminPage.clickJobBtn();
+        adminPage.clickJobTitleLink();
+        boolean verifyJobTitleDisplayed = adminPage.verifyJobTitleExistance();
+        assertFalse(verifyJobTitleDisplayed);
+    }
+    @Test
+    public void failEditJobTitle() throws InterruptedException {
+        boolean displayAdminLink = adminPage.checkAdminLink();
+        assertTrue(displayAdminLink);
+        adminPage.clickAdminLink();
+        adminPage.clickJobBtn();
+        adminPage.clickJobTitleLink();
+        boolean verifyJobTitleDisplayed = adminPage.verifyJobTitleExistance();
+        assertTrue(verifyJobTitleDisplayed);
+        adminPage.clickEditJobTitleBtn();
+        //adminPage.inputJobDescInput("Responsible for monitoring and improve an organization's final products.");
+        adminPage.clearInputJobTitle();
+        adminPage.clickSaveBtn();
+        adminPage.errorDisplayed();
+    }
+    @Test
+    public void successfulEditJobTitle() throws InterruptedException {
+        boolean displayAdminLink = adminPage.checkAdminLink();
+        assertTrue(displayAdminLink);
+        adminPage.clickAdminLink();
+        adminPage.clickJobBtn();
+        adminPage.clickJobTitleLink();
+        boolean verifyJobTitleDisplayed = adminPage.verifyJobTitleExistance();
+        assertTrue(verifyJobTitleDisplayed);
+        adminPage.clickEditJobTitleBtn();
+        adminPage.inputJobDescInput("Responsible for monitoring and improve an organization's final products.");
+        adminPage.clickSaveBtn();
+        boolean successfullyEditMsg = adminPage.verifySuccessfullyUpdate();
+        assertTrue(successfullyEditMsg);
+    }
+
+
 }
