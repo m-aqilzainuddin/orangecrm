e767ea3f2f9f544e140744dfde9422262b231756
m-aqilzainuddin <muhdaqilzainuddin@gmail.com>
2023-11-20 16:39:19 +0800

    Update - update script

--- src/main/java/pages/ForgotPasswordPage.java
+++ src/main/java/pages/ForgotPasswordPage.java
@@ -4,12 +4,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class ForgotPasswordPage {
-    @FindBy(xpath = "div[class*=\"forgot\"] > p")
+    @FindBy(xpath = "div[class*='forgot'] > p")
     WebElement forgotPasswordLink;
 
     @FindBy(xpath = "//button[text()[normalize-space() = 'Reset Password']]")
     WebElement resetButtons;
-    @FindBy(xpath = "[class*=\"orangehrm-forgot-password-button--reset\"]")
+    @FindBy(xpath = "[class*='orangehrm-forgot-password-button--reset']")
     WebElement resetButton;
 
     @FindBy(xpath = "//button[text()[normalize-space() = 'Cancel']]")
