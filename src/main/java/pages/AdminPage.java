7d2009136c66c8e66a79c9ca58820520ff8987fe
m-aqilzainuddin <muhdaqilzainuddin@gmail.com>
2023-12-13 03:11:22 +0800

    Update - refactor the script

--- src/main/java/pages/AdminPage.java
+++ src/main/java/pages/AdminPage.java
@@ -17,101 +17,36 @@ public class AdminPage {
         PageFactory.initElements(driver, this);
     }
 
+    //GLOBAL
     @FindBy(xpath = "//a[contains(@href, 'viewAdminModule')]")
     WebElement adminLink;
-
-//    @FindBy(xpath = "//label[text()='Username']/../following-sibling::div/input")
-//    WebElement adminUsername;
-//
-//    @FindBy(xpath = "//div[text()='Admin']")
-//    WebElement adminUsernameResult;
-//
-//    @FindBy (xpath = "//p[text()='No Records Found']")
-//    WebElement adminErrorUsername;
-//
-//    @FindBy(xpath = "//button[text()[normalize-space()='Search']]")
-//    WebElement adminSearchButton;
-//
-//    @FindBy(xpath = "//button[text()[normalize-space()='Reset']]")
-//    WebElement adminResetButton;
-
-
-    @FindBy(xpath = "//span[text()[normalize-space()='Job']]")
-    WebElement jobLink;
-
-    @FindBy(xpath = "//a[text()[normalize-space()='Job Titles']]")
-    WebElement jobTitleLink;
-
+    @FindBy(xpath = "//div[contains(@class,'orangehrm-dialog-popup')]")
+    WebElement globalPopupConfirmation;
+    @FindBy(xpath = "//button[text()[normalize-space()='Save']]")
+    WebElement globalSaveButton;
+    @FindBy(xpath = "//button[text()[normalize-space()='Cancel']]")
+    WebElement globalCancelButton;
+    @FindBy(xpath = "//button[text()[normalize-space()='Yes, Delete']]")
+    WebElement globalConfirmDltBtn;
+    @FindBy(xpath = "//button[text()[normalize-space()='No, Cancel']]")
+    WebElement globalConfirmCancelBtn;
+    @FindBy(xpath = "//span[text()[normalize-space()='Required']]")
+    WebElement globalInputRequired;
+    @FindBy(xpath = "//p[text()[normalize-space()='Successfully Saved']]")
+    WebElement globalSuccessfullySaveMsg;
+    @FindBy(xpath = "//p[text()[normalize-space()='Successfully Updated']]")
+    WebElement globalSuccessfullyUpdateMsg;
+    @FindBy(xpath = "//p[text()[normalize-space()='Successfully Deleted']]")
+    WebElement globalSuccessfullyDltMsg;
     @FindBy(xpath = "//button[text()[normalize-space()='Add']]")
     WebElement globalAddBtn;
 
-    @FindBy(xpath = "//label[text()='Job Title']/../following-sibling::div/input")
-    WebElement jobTitleName;
-
-    @FindBy(xpath = "//label[text()='Job Description']/../following-sibling::div/textarea")
-    WebElement jobTitleDesc;
-
-    @FindBy(xpath = "//span[text()='Show More']")
-    WebElement jobTitleDescMore;
-
-    @FindBy(xpath = "//div[text()='Quality Assurance']/../following-sibling::div//span[text()=\"Responsible for monitoring and improve an organization's final products.\"]")
-    WebElement jobTitleDescDisplayed;
-
-    @FindBy(xpath = "//span[text()[normalize-space()='Required']]")
-    WebElement globalInputRequired;
-
-    @FindBy(xpath = "//p[text()[normalize-space()='Successfully Saved']]")
-    WebElement globalSuccessfullySaveMsg;
-
-    @FindBy(xpath = "//p[text()[normalize-space()='Successfully Updated']]")
-    WebElement globalSuccessfullyUpdateMsg;
-
-    @FindBy(xpath = "//div[text()[normalize-space()='Quality Assurance']]")
-    List<WebElement> jobTitleDisplayed;
-
-    @FindBy(xpath = "//div[text()[normalize-space()='Quality Assurance']]/../following-sibling::div//i[contains(@class,'bi-pencil-fill')]")
-    WebElement jobTitleEditBtn;
-
-    @FindBy(xpath = "//div[text()[normalize-space()='Quality Assurance']]/../following-sibling::div//i[contains(@class,'bi-trash')]")
-    WebElement jobTitleDltBtn;
-
-    @FindBy(xpath = "//div[contains(@class,'orangehrm-dialog-popup')]")
-    WebElement globalPopupConfirmation;
-
-    @FindBy(xpath = "//button[text()[normalize-space()='Save']]")
-    WebElement globalSaveButton;
-
-    @FindBy(xpath = "//button[text()[normalize-space()='Cancel']]")
-    WebElement globalCancelButton;
-
-    @FindBy(xpath = "//button[text()[normalize-space()='Yes, Delete']]")
-    WebElement globalConfirmDltBtn;
-
-    @FindBy(xpath = "//button[text()[normalize-space()='No, Cancel']]")
-    WebElement globalConfirmCancelBtn;
-
-
-//    public void enterSystemUsername(String username){
-//        adminUsername.click();
-//        adminUsername.sendKeys(username);
-//    }
-//    public void clickSearchButton(){
-//        adminSearchButton.click();
-//    }
-//    public void clickResetButton(){
-//        adminResetButton.click();
-//    }
+    //GLOBAL METHODS
+    public boolean adminLinkDisplayed(){
+        return adminLink.isDisplayed();
+    }
     public void clickCancelButton(){
         globalCancelButton.click();
-    }
-//    public boolean usernameDisplayed(){
-//        return adminUsernameResult.isDisplayed();
-//    }
-//    public boolean usernameNotDisplayed(){
-//        return adminErrorUsername.isDisplayed();
-//    }
-    public void clickJobBtn(){
-        jobLink.click();
     }
     public boolean verifySuccessAddMsg(){
         return globalSuccessfullySaveMsg.isDisplayed();
@@ -119,15 +54,69 @@ public class AdminPage {
     public boolean verifySuccessfullyUpdate(){
         return globalSuccessfullyUpdateMsg.isDisplayed();
     }
-    public boolean adminLinkDisplayed(){
-        return adminLink.isDisplayed();
+    public void clickSaveBtn(){
+        globalSaveButton.click();
     }
-    public void clickAdmin(){
-        boolean isDisplay = adminLink.isDisplayed();
-        if(isDisplay){
-            adminLink.click();
-        }
+    public boolean errorDisplayed(){
+        return globalInputRequired.isDisplayed();
     }
+
+    //SYSTEM USERS
+    @FindBy(xpath = "//label[text()='Username']/../following-sibling::div/input")
+    WebElement adminUsername;
+
+    @FindBy(xpath = "//div[text()='Admin']")
+    WebElement adminUsernameResult;
+
+    @FindBy (xpath = "//p[text()='No Records Found']")
+    WebElement adminErrorUsername;
+
+    @FindBy(xpath = "//button[text()[normalize-space()='Search']]")
+    WebElement adminSearchButton;
+
+    @FindBy(xpath = "//button[text()[normalize-space()='Reset']]")
+    WebElement adminResetButton;
+
+    //SYSTEM USERS METHODS
+    public void enterSystemUsername(String username){
+        adminUsername.click();
+        adminUsername.sendKeys(username);
+    }
+    public void clickSearchButton(){
+        adminSearchButton.click();
+    }
+    public void clickResetButton(){
+        adminResetButton.click();
+    }
+
+    public boolean usernameDisplayed(){
+        return adminUsernameResult.isDisplayed();
+    }
+    public boolean usernameNotDisplayed(){
+        return adminErrorUsername.isDisplayed();
+    }
+
+    //JOB TITLE
+    @FindBy(xpath = "//span[text()[normalize-space()='Job']]")
+    WebElement jobLink;
+    @FindBy(xpath = "//a[text()[normalize-space()='Job Titles']]")
+    WebElement jobTitleLink;
+    @FindBy(xpath = "//label[text()='Job Title']/../following-sibling::div/input")
+    WebElement jobTitleName;
+    @FindBy(xpath = "//label[text()='Job Description']/../following-sibling::div/textarea")
+    WebElement jobTitleDesc;
+    @FindBy(xpath = "//span[text()='Show More']")
+    WebElement jobTitleDescMore;
+    @FindBy(xpath = "//div[text()='Quality Assurance']/../following-sibling::div//span[text()=\"Responsible for monitoring and improve an organization's final products.\"]")
+    WebElement jobTitleDescDisplayed;
+    @FindBy(xpath = "//div[text()[normalize-space()='Quality Assurance']]")
+    List<WebElement> jobTitleDisplayed;
+    @FindBy(xpath = "//div[text()[normalize-space()='Quality Assurance']]/../following-sibling::div//i[contains(@class,'bi-pencil-fill')]")
+    WebElement jobTitleEditBtn;
+    @FindBy(xpath = "//div[text()[normalize-space()='Quality Assurance']]/../following-sibling::div//i[contains(@class,'bi-trash')]")
+    WebElement jobTitleDltBtn;
+
+    //JOB TITLE METHODS
     public void clickJobTitle(){
         boolean isDisplay = adminLink.isDisplayed();
         if(isDisplay){
@@ -139,15 +128,6 @@ public class AdminPage {
     public void inputJobDescInput(String jobDesc){
         jobTitleDesc.click();
         jobTitleDesc.sendKeys(jobDesc);
-    }
-    public void clickSaveBtn(){
-        globalSaveButton.click();
-    }
-    public boolean errorDisplayed(){
-        return globalInputRequired.isDisplayed();
-    }
-    public void clickEditJobTitleBtn(){
-        jobTitleEditBtn.click();
     }
     public void clickJobTitleDescMoreBtn(){
         jobTitleDescMore.click();
@@ -217,10 +197,6 @@ public class AdminPage {
     WebElement payGradeAddBtn;
     @FindBy(xpath = "//label[text()[normalize-space()='Name']]/../following-sibling::div/input")
     WebElement payGradeName;
-    @FindBy(xpath = "//button[text()[normalize-space()='Save']]")
-    WebElement payGradeSaveBtn;
-    @FindBy(xpath = "//button[text()[normalize-space()='Cancel']]")
-    WebElement payGradeCancelBtn;
     @FindBy(xpath = "//div[text()[normalize-space()='Grade 40']]/../following-sibling::div//i[contains(@class,'bi-trash')]")
     WebElement payGradeDltRecord;
     @FindBy(xpath = "//div[text()[normalize-space()='Grade 40']]/../following-sibling::div//i[contains(@class,'bi-pencil-fill')]")
@@ -237,18 +213,12 @@ public class AdminPage {
     WebElement payGradeMinSalary;
     @FindBy(xpath = "//label[text()[normalize-space()='Maximum Salary']]/../following-sibling::div/input")
     WebElement payGradeMaxSalary;
-    @FindBy(xpath = "//p[text()='Successfully Saved']")
-    WebElement payGradeCurrSuccessMsg;
     @FindBy(xpath = "//div[text()[normalize-space()='Malaysian Ringgit']]")
     List<WebElement> payGradeRecordDisplay;
-    @FindBy(xpath = "//div[text()[normalize-space()='Malaysian Ringgit']]/../following-sibling::div//i[contains(@class,'bi-trash')]")
-    WebElement payGradeRecordDlt;
-    @FindBy(xpath = "//div[text()[normalize-space()='Malaysian Ringgit']]/../following-sibling::div//i[contains(@class,'bi-pencil-fill')]")
-    WebElement payGradeRecordEdit;
-
     @FindBy(xpath = "//div[text()[normalize-space()='Grade 40']]")
     List<WebElement> payGradeDisplayed;
 
+    //PAY GRADE METHODS
     public void clickPayGrade(){
         boolean isExist = adminLink.isDisplayed();
         if(isExist) {
@@ -290,7 +260,27 @@ public class AdminPage {
         payGradeMinSalary.sendKeys("1");
         payGradeMaxSalary.sendKeys("1000000");
         payGradeCurrSaveBtn.click();
-        payGradeCurrSuccessMsg.isDisplayed();
+        globalSuccessfullyUpdateMsg.isDisplayed();
+    }
+    public void cancelUpdatePayGrade(){
+        payGradeEditRecord.click();
+        globalAddBtn.click();
+        payGradeCurrency.click();
+        payGradeCurrMYR.click();
+        payGradeMinSalary.sendKeys("1");
+        payGradeMaxSalary.sendKeys("1000000");
+        payGradeCurrCancelBtn.click();
+    }
+    public void cancelDltPayGrade(){
+        payGradeDltRecord.click();
+        globalPopupConfirmation.isDisplayed();
+        globalConfirmCancelBtn.click();
+    }
+    public void successDltPayGrade(){
+        payGradeDltRecord.click();
+        globalPopupConfirmation.isDisplayed();
+        globalConfirmDltBtn.click();
+        globalSuccessfullyDltMsg.isDisplayed();
     }
     public boolean verifyPayGradeRecordExist(){
         int count = payGradeRecordDisplay.size();
@@ -301,11 +291,12 @@ public class AdminPage {
         return count > 0;
     }
 
-
-
-
-
     //EMPLOYEE STATUS
 
+    //EMPLOYEE STATUS METHODS
+
+
     //JOB CATEGORY
+
+    //JOB CATEGORY METHODS
 }
